f616a0aa150978c6242c0a8793373f469a94b339
Aariz4ever <135448039+Aariz4ever@users.noreply.github.com>
2024-09-17 17:44:46 +0530

    Added user app and dvc pipeline

--- src/cnnClassifier/pipeline/stage_01_data_ingestion.py
+++ src/cnnClassifier/pipeline/stage_01_data_ingestion.py
@@ -1,13 +1,15 @@
-from cnnClassifier.components.data_ingestion import DataIngestion
 from cnnClassifier.config.configuration import ConfigurationManager
+from cnnClassifier.components.data_ingestion import DataIngestion
 from cnnClassifier import logger
+
 
 
 STAGE_NAME = "Data Ingestion stage"
 
 class DataIngestionTrainingPipeline:
-    def __init__(self) -> None:
+    def __init__(self):
         pass
+
     def main(self):
         config = ConfigurationManager()
         data_ingestion_config = config.get_data_ingestion_config()
@@ -16,12 +18,14 @@ class DataIngestionTrainingPipeline:
         data_ingestion.extract_zip_file()
 
 
-if __name__=='__main__':
+
+
+if __name__ == '__main__':
     try:
         logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
         obj = DataIngestionTrainingPipeline()
         obj.main()
-        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx=========x")
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
     except Exception as e:
         logger.exception(e)
         raise e
